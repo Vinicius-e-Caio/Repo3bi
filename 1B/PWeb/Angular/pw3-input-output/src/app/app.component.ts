c0ebc4fff2d058916b4821f19bc860c8442b49cb
Vinicius-e-Caio <viniciusjesusfilho05@gmail.com>
2025-03-18 14:31:11 -0300

    feat: added today's class content and assessment material

--- 1B/PWeb/Angular/pw3-input-output/src/app/app.component.ts
+++ 1B/PWeb/Angular/pw3-input-output/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextOverflowComponent } from "./text-overflow/text-overflow.component";
+import { AleatorioComponent } from "./aleatorio/aleatorio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TextOverflowComponent],
+  imports: [RouterOutlet, TextOverflowComponent, AleatorioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pw3-input-output';
+  resultado: string = ""
+  processar(valor:string) {
+    this.resultado = valor
+  }
 }
