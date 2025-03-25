914536d53c278fc1e23754ec1dc75908669f1977
Vinicius-e-Caio <viniciusjesusfilho05@gmail.com>
2025-03-25 14:39:08 -0300

    feat: function to reload phrases && styles

--- 1B/PWeb/Atividade1/pw3-biscoito/src/app/botao/botao.component.ts
+++ 1B/PWeb/Atividade1/pw3-biscoito/src/app/botao/botao.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FrasesComponent } from '../frases/frases.component';
 
 @Component({
   selector: 'app-botao',
-  imports: [],
+  imports: [ FrasesComponent ],
   templateUrl: './botao.component.html',
   styleUrl: './botao.component.css'
 })
--- 1B/PWeb/Atividade1/pw3-biscoito/src/app/frases/frases.component.ts
+++ 1B/PWeb/Atividade1/pw3-biscoito/src/app/frases/frases.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-frases',
@@ -8,17 +8,11 @@ import { Component, input, Input } from '@angular/core';
 })
 export class FrasesComponent {
   @Input() ativo: boolean = false;
-  phrases: string[] = [
-    "Seja forte e corajoso! — Josué 1:9",
-    "O amor é paciente, o amor é bondoso. — 1 Coríntios 13:4",
-    "Em tudo dai graças. — 1 Tessalonicenses 5:18",
-    "Não andeis ansiosos por coisa alguma. — Filipenses 4:6",
-    "Tudo coopera para o bem daqueles que amam a Deus. — Romanos 8:28",
-    "Busque a sabedoria e ela te exaltará. — Provérbios 4:8",
-    "Se Deus é por nós, quem será contra nós? — Romanos 8:31",
-    "A verdade vos libertará. — João 8:32",
-    "Tudo tem o seu tempo. — Eclesiastes 3:1",
-    "Sejam imitadores de Deus — Efésios 5:1-2",
-  ]
-  frase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+  @Input() frase:string = "";
+
+  // @Output() messageEvent = new EventEmitter<string>();
+  // newPhrase() {
+  //   this.frase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+  // }
+
 }
