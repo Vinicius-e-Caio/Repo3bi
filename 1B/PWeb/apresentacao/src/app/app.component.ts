ca8f2d4a9f769a740689384c2b31913a41f078cf
Vinicius-e-Caio <viniciusjesusfilho05@gmail.com>
2025-04-08 12:03:35 -0300

    feat: my history and new fixes

--- 1B/PWeb/apresentacao/src/app/app.component.ts
+++ 1B/PWeb/apresentacao/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TesteComponent } from './teste/teste.component';
 import { CaioComponent } from './caio/caio.component';
+import { ViniComponent } from "./vini/vini.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TesteComponent, CaioComponent],
+  imports: [RouterOutlet, TesteComponent, CaioComponent, ViniComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 1B/PWeb/apresentacao/src/app/vini/vini.component.ts
+++ 1B/PWeb/apresentacao/src/app/vini/vini.component.ts
@@ -1,14 +1,19 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatChipsModule} from '@angular/material/chips';
 
 
 @Component({
   selector: 'app-vini',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatChipsModule, MatProgressBarModule],
   templateUrl: './vini.component.html',
   styleUrl: './vini.component.css'
 })
 export class ViniComponent {
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
 
-}
+} 
