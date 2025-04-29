b38b318c0aaa3ac139278491e6d7604b397add19
Vinicius-e-Caio <viniciusjesusfilho05@gmail.com>
2025-04-29 14:42:20 -0300

    feat: content task

--- 2B/PWeb/pw3-diretivas/src/app/tarefas/tarefas.component.ts
+++ 2B/PWeb/pw3-diretivas/src/app/tarefas/tarefas.component.ts
@@ -7,14 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './tarefas.component.css'
 })
 export class TarefasComponent {
-  Temp: boolean = true
-  tarefas: any[] = [{id: 1, title:"arrumar a casa", finished : "- concluída"},
-                    {id: 2, title:"tarefa pw", finished : ""},
-                    {id: 3, title:"arrumar as malas", finished : ""},
-                    {id: 4, title:"comprar passagem", finished : "- concluída"},
-                    {id: 5, title:"preparar a comida", finished : ""},
-                    {id: 6, title:"devocional", finished : "- concluída"}];
-  showThis () {
-    this.Temp = !this.Temp
+  // Temp: boolean = true
+  // tarefas: any[] = [{id: 1, title:"arrumar a casa", finished : "- concluída"},
+  //                   {id: 2, title:"tarefa pw", finished : ""},
+  //                   {id: 3, title:"arrumar as malas", finished : ""},
+  //                   {id: 4, title:"comprar passagem", finished : "- concluída"},
+  //                   {id: 5, title:"preparar a comida", finished : ""},
+  //                   {id: 6, title:"devocional", finished : "- concluída"}];
+  // showThis () {
+  //   this.Temp = !this.Temp
+  // }
+  exibirTudo: boolean = true
+  tarefas = [
+    {"id": 1, "nome": "Arrumar a casa", concluida : true},
+    {"id": 2, "nome": "Devocional", concluida : false},
+    {"id": 3, "nome": "Estudo", concluida : true},
+    {"id": 4, "nome": "Comida", concluida : false}
+  ]
+  onExibirTudo() {
+    this.exibirTudo = !this.exibirTudo
   }
 }
