a6b65622869560cd17bc863fe85fd29c7a02c8c0
Vinicius-e-Caio <viniciusjesusfilho05@gmail.com>
2025-08-19 14:36:54 -0300

    chore: pw3-ui

--- 3B/Pw3Project/pw3-tarefas/src/main/java/br/com/etechas/tarefas/controller/TarefaController.java
+++ 3B/Pw3Project/pw3-tarefas/src/main/java/br/com/etechas/tarefas/controller/TarefaController.java
@@ -4,14 +4,18 @@ import br.com.etechas.tarefas.entity.Tarefa;
 import br.com.etechas.tarefas.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/tarefas")
 public class TarefaController {
     @Autowired
     private TarefaService service;
 
-    @GetMapping
+    @GetMapping("/listar")
     public List<Tarefa> listar(){
         return service.listarTarefas();
     }
